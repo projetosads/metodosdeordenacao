6c348565b634d435f06a86d217574b9b2324e75c
metodos de ordenação <49003243+projetosads@users.noreply.github.com>
2019-10-17 14:31:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body bgcolor="FFFF00">
+<center><h2>Cadastro Cliente</h2></center>
+
+
+	<form action="cadastro.php" method="post">
+		Cliente: <br>
+		<input type="text" name="nome"> <br><br>
+		Endereco: <br>
+		<input type="text" name="endereco"> <br><br>
+
+Telefone: <br>
+		<input type="text" name="telefone"> <br><br>
+Email: <br>
+		<input type="email" name="email"> <br><br>
+		<input type="submit" value="Cadastrar">
+	</form>
+<h3>
+<a href="listar.php">Listar Clientes</a> </h1>
+	</h3>
+</body>
+</html>
